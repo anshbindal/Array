e93dbea7e9fb2a18c85ebcb7135fc29f5595b22a
anshbindal <78355426+anshbindal@users.noreply.github.com>
2022-06-22 18:34:02 +0530

    Created an array

---
+++ array.js
@@ -0,0 +1,6 @@
+//An array is a data structure that contains a group of elements. Typically these elements are all of the same data type, such as an integer or string. Arrays are commonly used in computer programs to organize data so that a related set of values can be easily sorted or searched.
+
+
+
+
+const numbers = [1, 2, 3]
